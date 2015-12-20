ed3201e150b7d89d69e39aa0335fbf4c2f92dd6b
Bloodcount <searosss@gmail.com>
2015-12-20 22:15:18 +0200

    added draft for cdn

--- cdn/cdn.go
+++ cdn/cdn.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	)
 
 func main() {
             fmt.Printf("Hello, world!\n")
+	    http.Handle("/cdn/", http.StripPrefix("/cdn", http.FileServer(http.Dir("<path>"))))
+	    http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
+				// todo add insert method
+				fmt.Fprintf(w, "Hello, %q", r.URL.Path)
+	    })
+	    //todo maybe add remove method? 
+	    http.ListenAndServe(":6543", nil)
 }
 
