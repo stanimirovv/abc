299c4b5faf9cf9b1fb791f212bcbf69bf3a490d3
Bloodcount <searosss@gmail.com>
2015-12-25 21:10:42 +0200

    initial commit for xerrors

---
+++ cdn/xerrors.go
@@ -0,0 +1,13 @@
+package main
+
+/*
+* This file contains the custom errors and logging that will be used in the project.
+* It will eventually become a package, when things have been figured out, until then it will be a "plug and play" file.
+*/
+
+
+import ( "fmt")
+
+func printHello(){
+   fmt.Printf("Hello, world from zlatin!\n") 
+}
