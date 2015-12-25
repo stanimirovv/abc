723240975bbe5f928394759e4b20a21bfb97b925
Bloodcount <searosss@gmail.com>
2015-12-25 22:54:06 +0200

    feat: first version

--- cdn/xerrors.go
+++ cdn/xerrors.go
@@ -6,8 +6,23 @@ package main
 */
 
 
-import ( "fmt")
 
-func printHello(){
-   fmt.Printf("Hello, world from zlatin!\n") 
+/*
+* The system errors are FATAL, UNRECOVERABLE errors in the BUSINESS logic of the application.
+* The current execution path must be aborted. 
+* If the process ends or is freed to start working on the next job is up to the developer.
+*/
+
+type SysErr struct {
+    msg string
 }
+
+func (e *SysErr) Error() string {
+    //return fmt.Sprintf(" %s", e.prob)
+    return  "Application Error!"
+}
+
+func NewSysErr(msg string) SysErr{
+    return SysErr{msg}
+}
+
