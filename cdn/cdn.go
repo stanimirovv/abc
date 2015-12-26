261ccb4211ebd4a67315b95d229be2aeaa118aa2
Bloodcount <searosss@gmail.com>
2015-12-26 12:57:58 +0200

    chore: added todos

--- cdn/cdn.go
+++ cdn/cdn.go
@@ -7,20 +7,42 @@ import (
 	"os"
 	"mime/multipart"
 	"io"
+	"strconv"
 	)
 
 func main() {
     flag.Parse()
 
-    /* TODO add parametrization */
-    glog.Info("Starting File Server on port 6543")
+    /* TODO add parametrization 
+	- port number
+	- server path to cdn TODO
+	- server base url 
+	- server upload url TODO
+	- cdn dir
+    */
+
+    /*
+    * issues: files with same name can be overridden
+    * there is literally no security
+    * They will be solved on the second iteration of the cdn
+    */
+
+    a := os.Getenv("GOPATH1")
+    glog.Info("AAA: ", a)
+    portNumber, err := strconv.Atoi(os.Getenv("ABC_CDN_PORTNUM"))
+    if nil != err {
+	glog.Fatal("Error: ", err, portNumber)
+    }
+
+
+
+    glog.Info(`Starting File Server on port`, os.Getenv("ABC_CDN_PORTNUM"))
     http.Handle("/cdn/", http.StripPrefix("/cdn", http.FileServer(http.Dir("."))))
     glog.Info("Starting API Server on port 6543 path")
     http.HandleFunc("/api/upload", func(res http.ResponseWriter, req *http.Request) {
 				var err error
 				defer func() {
 				    if nil != err {
-					//http.Error(res, err.Error(), status)
 					glog.Error("Error string: errStr", err)
 					res.Write([]byte(`{"status":"error", "message":"` + err.Error()  + `"}`))
 				    }
@@ -45,17 +67,15 @@ func main() {
 					if nil != err {
 					     return
 					}
-					// 32K buffer copy  
 					var written int64
 					written, err = io.Copy(outfile, infile)
 					if nil != err || 0 == written {
 					     return
 					}
-					//res.Write([]byte("uploaded file:" + hdr.Filename + ";length:" + strconv.Itoa(int(written))))
 					res.Write([]byte(`{"status":"ok", "resource_url":"` + `path_to_storage` + hdr.Filename + `"}`))
 				    }
 				}
     })
-    http.ListenAndServe(":6543", nil)
+    http.ListenAndServe(`:` + os.Getenv("ABC_CDN_PORTNUM"), nil)
 }
 
