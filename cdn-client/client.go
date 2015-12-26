51eb1fc245e53f560c19709d30952ff402046370
Bloodcount <searosss@gmail.com>
2015-12-26 13:28:05 +0200

    chore: update api path and test file's name

--- cdn-client/client.go
+++ cdn-client/client.go
@@ -53,7 +53,7 @@ func postFile(filename string, targetUrl string) error {
 
 // sample usage
 func main() {
-    target_url := "http://localhost:6543/api/upload"
-    filename := "./a.webm"
+    target_url := "http://localhost:6540/api/upload"
+    filename := "a.webm"
     postFile(filename, target_url)
 }
