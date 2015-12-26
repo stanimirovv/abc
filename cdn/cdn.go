178830bccb3de942b196f2e11375727c7a80be51
Bloodcount <searosss@gmail.com>
2015-12-26 11:59:52 +0200

    feat: added error checking

--- cdn/cdn.go
+++ cdn/cdn.go
@@ -7,30 +7,28 @@ import (
 	"os"
 	"mime/multipart"
 	"io"
-	"strconv"
 	)
 
 func main() {
     flag.Parse()
 
+    /* TODO add parametrization */
     glog.Info("Starting File Server on port 6543")
     http.Handle("/cdn/", http.StripPrefix("/cdn", http.FileServer(http.Dir("."))))
     glog.Info("Starting API Server on port 6543 path")
-    /* TODO add error handling! */
     http.HandleFunc("/api/upload", func(res http.ResponseWriter, req *http.Request) {
-				var (
-				    status int
-				    err  error
-				)
+				var err error
 				defer func() {
 				    if nil != err {
-					http.Error(res, err.Error(), status)
+					//http.Error(res, err.Error(), status)
+					glog.Error("Error string: errStr", err)
+					res.Write([]byte(`{"status":"error", "message":"` + err.Error()  + `"}`))
 				    }
 				}()
 				// max bytes in mem at a time  
 				const _24K = (1 << 20) * 24
-				if err = req.ParseMultipartForm(_24K); nil != err {
-				    status = http.StatusInternalServerError
+				err = req.ParseMultipartForm(_24K) 
+				if nil != err {
 				    return
 				}
 				for _, fheaders := range req.MultipartForm.File {
@@ -39,24 +37,22 @@ func main() {
 					var infile multipart.File
 					infile, err = hdr.Open()
 					if nil != err {
-					     status = http.StatusInternalServerError
 					     return
 					}
 					// open destination  
 					var outfile *os.File
 					outfile, err = os.Create(hdr.Filename)
 					if nil != err {
-					     status = http.StatusInternalServerError
 					     return
 					}
 					// 32K buffer copy  
 					var written int64
 					written, err = io.Copy(outfile, infile)
-					if nil != err {
-					     status = http.StatusInternalServerError
+					if nil != err || 0 == written {
 					     return
 					}
-					res.Write([]byte("uploaded file:" + hdr.Filename + ";length:" + strconv.Itoa(int(written))))
+					//res.Write([]byte("uploaded file:" + hdr.Filename + ";length:" + strconv.Itoa(int(written))))
+					res.Write([]byte(`{"status":"ok", "resource_url":"` + `path_to_storage` + hdr.Filename + `"}`))
 				    }
 				}
     })
