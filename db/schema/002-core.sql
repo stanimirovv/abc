49fd5fe3f6686d912436e6043dbbb5473ddc7235
Bloodcount <searosss@gmail.com>
2015-12-27 23:15:01 +0200

    feat: added WIP db

---
+++ db/schema/002-core.sql
@@ -0,0 +1,46 @@
+
+
+CREATE TABLE image_board_clusters(id serial primary key,
+                                  name text not null,
+                                  descr text not null,
+                                  long_descr text,
+                                  board_limit_count INT NOT NULL default 99999
+                                  );
+
+CREATE TABLE thread_limits_reached_actions(
+                                            id serial primary key,
+                                            name text not null,
+                                            descr text
+);
+
+-- archive
+-- delete
+-- read only
+
+CREATE TABLE boards(id serial primary key,
+                    name text not null,
+                    descr text,
+                    thread_setting_max_thread_count INT NOT NULL default -1,
+                    thread_setting_max_posts_per_thread INT NOT NULL default 999999,
+                    thread_setting_are_attachments_allowed BOOLEAN NOT NULL DEFAULT FALSE,
+                    thread_setting_limits_reached_action_id INT REFERENCES thread_limits_reached_actions
+                    -- Board total posts count ?
+                    -- Board total attachments count ? 
+                    -- Max views ? 
+                    -- Time expires at
+                    -- Date expires at
+                    );
+
+CREATE TABLE threads(   id serial primary key,
+                        name text not null,
+                        board_id INT REFERENCES boards,
+                        max_posts_per_thread INT NOT NULL default 999999,
+                        are_attachments_allowed BOOLEAN NOT NULL DEFAULT FALSE,
+                        limits_reached_action_id INT REFERENCES thread_limits_reached_actions
+                    );
+
+CREATE TABLE thread_posts(id serial primary key,
+                          body text not null,
+                          thread_id INT REFERENCES threads,
+                          attachment_url TEXT
+                          -- todo limits);
