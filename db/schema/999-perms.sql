554e8ceacb6587ad577f61e074d483e90a4a47ea
Bloodcount <searosss@gmail.com>
2015-12-30 15:21:21 +0200

    feat: permissions for api_user

---
+++ db/schema/999-perms.sql
@@ -0,0 +1,13 @@
+CREATE USER abc_api WITH PASSWORD '123';
+GRANT SELECT    ON thread_posts         TO abc_api; 
+GRANT INSERT    ON thread_posts         TO abc_api; 
+GRANT USAGE     ON thread_posts_id_seq  TO abc_api;
+GRANT SELECT    ON threads              TO abc_api;
+GRANT INSERT    ON threads              TO abc_api;
+GRANT USAGE     ON threads_id_seq       TO abc_api;
+GRANT SELECT    ON boards               TO abc_api;
+GRANT USAGE     ON boards_id_seq        TO abc_api;
+GRANT SELECT    ON image_board_clusters TO abc_api;
+GRANT USAGE     ON image_board_clusters_id_seq TO acd_api;
+GRANT SELECT    ON thread_limits_reached_actions TO abc_api;
+GRANT USAGE     ON thread_limits_reached_actions_id_seq TO abc_api;
