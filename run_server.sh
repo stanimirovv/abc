8bab8e21059aca4ba0648c5d4a7c84397224a89a
Bloodcount <searosss@gmail.com>
2016-03-28 21:39:47 +0300

    feat: relative path to client

--- run_server.sh
+++ run_server.sh
@@ -2,5 +2,7 @@
 
 export ABC_SERVER_ENDPOINT_URL="8089"
 export ABC_DB_CONN_STRING="user=abc_api password=123 dbname=abc_dev_cluster sslmode=disable" 
+export ABC_FILES_DIR="./client/"
+
 # Run file for the project
 /usr/local/go/bin/go run server/server.go -logtostderr=true
