ce22b366c0f33a348a8d464e1580a93f08ee7dbb
Bloodcount <searosss@gmail.com>
2016-04-03 18:35:21 +0300

    refactor: removed struct definitions

--- server/server.go
+++ server/server.go
@@ -19,65 +19,9 @@ import (
 	_ "github.com/lib/pq"
 )
 
-/*
-TODO:
-2) Add different input/output formats for the API
-6) quote of the day
-*/
-
 // LAST STATUS ID: 20
 
-type image_board_clusters struct {
-    Id int
-    Descr string
-    LongDescr string
-    BoardLimitCount int
-}
 
-type boards struct {
-    Id int
-    Name string
-    Descr string
-    ImageBoardClusterId string
-    MaxThreadCount int //to be checked in insert thread
-    MaxActiveThreadCount int //to be checked  in insert thread
-    MaxPostsPerThread int // to be checked in insert thread
-    AreAttachmentsAllowed bool // to be checked in insert post
-    PostLimitsReachedActionId int // to be checked in insert post
-}
-
-type threads struct{
-    Id int
-    Name string
-    Descr string
-    BoardId int
-    MaxPostsPerThread int
-    AreAttachmentsAllowed bool
-    LimitsReachedActionId int
-    PostCount int
-    PostCountWithAttachment int
-}
-
-type thread_posts struct{
-    Id int
-    Body string
-    ThreadId int
-    AttachmentUrl *string
-    InsertedAt time.Time
-    SourceIp *string
-}
-
-type thread_limits_reached_actions struct{
-    Id	    int
-    Name    string
-    Descr   string
-}
-
-type api_request struct{
-    Status  string
-    Msg	    *string
-    Payload interface{}
-}
 
 
 func getBoards(res http.ResponseWriter, req *http.Request)  ([]byte, error) {
