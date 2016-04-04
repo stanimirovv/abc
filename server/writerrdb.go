1059c765a3d82ba4c1c0be37d222ab3094e800bb
Bloodcount <searosss@gmail.com>
2016-04-04 23:07:08 +0300

    feat: implemented getBoards

--- server/writerrdb.go
+++ server/writerrdb.go
@@ -1,10 +1,35 @@
 package main
 
+import (
+	//DB
+	"database/sql"
+	_ "github.com/lib/pq"
+	"github.com/iambc/xerrors"
+)
+
+var dbh *sql.DB
+var dbConnString string
+
 //Relational database implementation for writer interface
 type writerrdb struct {
 }
 
-func (db *writerrdb) getBoards(apiKey string) []boards {
+func (db *writerrdb) getBoards(apiKey string) (currBoards []boards, err error) {
+    rows, err := dbh.Query("select b.id, b.name, b.descr from boards b join image_board_clusters ibc on ibc.id = b.image_board_cluster_id where api_key = $1;", apiKey)
+    if err != nil {
+	return currBoards, xerrors.NewUIErr(err.Error(), err.Error(), `002`, true)
+    }
+    defer rows.Close()
+
+    for rows.Next() {
+	var board boards
+	err = rows.Scan(&board.Id, &board.Name, &board.Descr)
+	if err != nil {
+	    return currBoards, xerrors.NewUIErr(err.Error(), err.Error(), `003`, true)
+	}
+	currBoards = append(currBoards, board)
+    }
+    return currBoards, nil
 }
 
 func (db *writerrdb) getActiveThreadsForBoard(apiKey string, boardId int) []threads {
