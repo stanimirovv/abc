c58123c20a4c8d90553505d73fcd752bf87de9fd
Bloodcount <searosss@gmail.com>
2016-04-05 22:26:21 +0300

    refactor:

--- run_server.sh
+++ run_server.sh
@@ -3,6 +3,7 @@
 export ABC_SERVER_ENDPOINT_URL="8089"
 export ABC_DB_CONN_STRING="user=abc_api password=123 dbname=abc_dev_cluster sslmode=disable" 
 export ABC_FILES_DIR="./client/"
+export ABC_FILES_SERVER_URL="8088"
 
 # Run file for the project
-/usr/local/go/bin/go run server/server.go -logtostderr=true
+./server/server -logtostderr=true
