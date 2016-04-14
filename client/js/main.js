b204f6e87ecbcf3f41d4f60ebfab4160d80c680b
Bloodcount <searosss@gmail.com>
2016-04-14 23:25:10 +0300

    refactor: wip: getPostsForThread

--- client/js/main.js
+++ client/js/main.js
@@ -4,12 +4,12 @@
 
     function locationHashChanged() {
         //console.log("Change: " + location.hash);
-        path = location.hash.split('/');
+        var path = location.hash.split('/');
         console.log("Path: ", path);
         if(path.length === 1) {
             getBoards();
         } else if(path.length === 2) {
-            board = path[1].split(':');
+            var board = path[1].split(':');
 
             if( board.length !== 2){
                 uiError('Bad path!');
@@ -17,15 +17,15 @@
                 getActiveThreadsForBoard(board[1]);
             }
         } else if (path.length === 3) {
-            board = path[1].split(':');
+            var board = path[1].split(':');
 
             if( board.length !== 2){
                 uiError('Bad path!');
                 return
             }
 
-            thread = path[1].split[':'];
-            if( params.length !== 2){
+            var thread = path[2].split(':');
+            if( thread.length !== 2){
                 uiError('Bad path!');
             } else {
                 getPostsForThread(board[1] ,thread[1]);
@@ -38,6 +38,7 @@
     window.onload = locationHashChanged;
 
  boards = {}
+ threads = {}
  function getTestPromise(){
     new Promise(function(resolve, reject) {
     $.ajax({
@@ -88,8 +89,8 @@ function getActiveThreadsForBoard(boardId){
             getBoards()
             resolve('ok');
         })
-        .then(function(e) { getActiveThreadsForBoardA(boardId) })
-        .catch(function(e) { console.log('catch: ', e); });
+        .then(function(e) { getActiveThreadsForBoardA(boardId) },
+                function(e) { console.log('catch: ', e); });
     }
 
 }
@@ -100,20 +101,19 @@ function getActiveThreadsForBoardA(boardId){
           url: "http://127.0.0.1:8089/api?command=getActiveThreadsForBoard&api_key=d3c3f756aff00db5cb063765b828e87b&board_id=" + boardId,
           type: "GET",
           success: function(resp){
-              respObj = JSON.parse(resp);
-              if( respObj.Status !== 'ok') {
+              threads = JSON.parse(resp);
+              if( threads.Status !== 'ok') {
                   alert(resp.Msg);
               }
-
               var html = '';
               for(var i = 0; i < boards.Payload.length; i++){
                   if(boards.Payload[i].ID == boardId){
                       html += '<h1>' + boards.Payload[i].Name +'</h1>';
                   }
               }
-              for (var i = 0; i < respObj.Payload.length; i++){
-                  console.log(respObj.Payload[i]);
-                  html += '<h2>'+ respObj.Payload[i].Name +'</h2>';
+              for (var i = 0; i < threads.Payload.length; i++){
+                  console.log(threads.Payload[i]);
+                  html += '<h2>'+ threads.Payload[i].Name +'</h2>';
               }
               $("#app").html(html);
           },
@@ -122,6 +122,7 @@ function getActiveThreadsForBoardA(boardId){
 }
 
 function getPostsForThread(boardId, threadId){
+
     console.log(boardId);
     if(boards.Status === undefined){
         console.log('MUST LOAD CACHE');
@@ -129,14 +130,35 @@ function getPostsForThread(boardId, threadId){
             getBoards()
             resolve('ok');
         })
-        .then(function(e) { getActiveThreadsForBoardA(boardId) })
-        .catch(function(e) { console.log('catch: ', e); });
+        .then(function(e) { getActiveThreadsForBoardA(boardId) }, function(e) { console.log('catch: ', e); })
+        .then(function(e){getPostsForThreadA(boardId, threadId)}, function(e) {console.log('catch: ', e); });
     }
+
 }
 
+
 function getPostsForThreadA(boardId, threadId){
+    $.ajax({
+              url: "http://127.0.0.1:8089/api?command=getPostsForThread&api_key=d3c3f756aff00db5cb063765b828e87b&thread_id=" + threadId,
+              type: "GET",
+              success: function(resp ){
+                        respObj = JSON.parse(resp);
+                        if( respObj.Status !== 'ok') {
+                            alert(resp.Msg);
+                        }
 
-
+                        var html = '';
+                        for (var i = 0; i < threads.Payload.length; i++){
+                            console.log(threads.Payload[i]);
+                            html += '<h2>'+ threads.Payload[i].Name +'</h2>';
+                        }
+                        for(var i =0; i < respObj.Payload.length; i++){
+                            html += '<div>' + respObj.Payload[i].Body + '</div>';
+                        }
+                        console.log(respObj);
+                    },
+              error: function(){reject("ERROR!");}
+          });
 }
 
 //xmlhttp.open("GET", "http://127.0.0.1:8089/api?command=getPostsForThread&api_key=d3c3f756aff00db5cb063765b828e87b&thread_id=" + threadId);
