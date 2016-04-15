dbeaed3b218c199bd37fe2d9c90a8960b486d46a
Bloodcount <searosss@gmail.com>
2016-04-15 17:27:01 +0300

    refactor: method names

--- client/js/main.js
+++ client/js/main.js
@@ -17,7 +17,7 @@
                 uiError('Bad path!');
             } else {
                 console.log("locationHashChanged: getActiveThreadsForBoard()");
-                getActiveThreadsForBoard(board[1]);
+                getActiveThreadsForBoardChain(board[1]);
             }
         } else if (path.length === 3) {
             var board = path[1].split(':');
@@ -32,7 +32,7 @@
                 uiError('Bad path!');
             } else {
                 console.log("locationHashChanged: getPostsForThread()");
-                getPostsForThread(board[1] ,thread[1]);
+                getPostsForThreadChain(board[1] ,thread[1]);
             }
         } else {
             console.log("Unknown path!");
@@ -88,7 +88,7 @@ function uiError(errorText){
     alert(errorText);
 }
 
-function getActiveThreadsForBoard(boardId){
+function getActiveThreadsForBoardChain(boardId){
     console.log("boards: ", boards);
     console.log(boardId);
     if(boards.Status === undefined){
@@ -97,15 +97,15 @@ function getActiveThreadsForBoard(boardId){
             getBoards()
             resolve('ok');
         })
-        .then(function(e) { getActiveThreadsForBoardA(boardId) },
+        .then(function(e) { getActiveThreadsForBoard(boardId) },
                 function(e) { console.log('catch: ', e); });
     } else {
-        getActiveThreadsForBoardA(boardId);
+        getActiveThreadsForBoardgetActiveThreadsForBoard(boardId);
     }
 
 }
 
-function getActiveThreadsForBoardA(boardId, resolve){
+function getActiveThreadsForBoard(boardId, resolve){
     console.log(arguments.callee.name);
     $.ajax({
           url: "http://127.0.0.1:8089/api?command=getActiveThreadsForBoard&api_key=d3c3f756aff00db5cb063765b828e87b&board_id=" + boardId,
@@ -129,28 +129,28 @@ function getActiveThreadsForBoardA(boardId, resolve){
                   console.log('resolving the promise');
                   resolve('ok');
               }
-              console.log('END getActiveThreadsForBoardA');
+              console.log('END getActiveThreadsForBoard');
           },
           error: function(){}
       });
 }
 
-function getPostsForThread(boardId, threadId){
+function getPostsForThreadChain(boardId, threadId){
     console.log(arguments.callee.name);
     if(boards.Status === undefined){
         console.log('MUST LOAD CACHE');
 
         new Promise(function(resolve, reject) {
             getBoards(resolve);})
-            .then(function(e) { return new Promise(function(resolve) {getActiveThreadsForBoardA(boardId, resolve);})}, function(e) { console.log('catch: ', e); })
-        .then(function(e){getPostsForThreadA(boardId, threadId);}, function(e) {console.log('catch: ', e); })
+            .then(function(e) { return new Promise(function(resolve) {getActiveThreadsForBoard(boardId, resolve);})}, function(e) { console.log('catch: ', e); })
+        .then(function(e){getPostsForThread(boardId, threadId);}, function(e) {console.log('catch: ', e); })
     } else {
-        getPostsForThreadA(boardId, threadId);
+        getPostsForThread(boardId, threadId);
     }
 }
 
 
-function getPostsForThreadA(boardId, threadId){
+function getPostsForThread(boardId, threadId){
     console.log(arguments.callee.name);
 
     $.ajax({
@@ -185,4 +185,3 @@ function getPostsForThreadA(boardId, threadId){
 //xmlhttp.open("GET", "http://localhost:8089/api?command=addPostToThread&api_key=d3c3f756aff00db5cb063765b828e87b&thread_id=" + threadId +
                         //"&thread_post_body=" + escape(document.getElementById('newPostTextArea').value) + "&attachment_url="+escape(attachmentUrl));
 //xmlhttp.open("GET", "http://localhost:8089/api?command=addThread&api_key=d3c3f756aff00db5cb063765b828e87b&board_id=" +boardId +"&thread_name="+ escape(document.getElementById('newThreadTextArea').value) );
-//xmlhttp1.open("GET", "http://localhost:8089/api?command=addPostToThread&api_key=d3c3f756aff00db5cb063765b828e87b&thread_id=" + addThreadResp.Payload.Id +
