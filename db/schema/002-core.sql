92d99399227771c67fe38bbd6be57ed3c793dad0
Bloodcount <searosss@gmail.com>
2016-04-17 22:05:28 +0300

    feat: added api table

--- db/schema/002-core.sql
+++ db/schema/002-core.sql
@@ -52,3 +52,11 @@ CREATE TABLE thread_posts(id serial primary key,
                           inserted_at timestamp with timezone not null default now(),
                           source_ip TEXT);
                           -- todo limits);
+CREATE TABLE apis(
+    id SERIAL PRIMARY KEY,
+    api_key TEXT NOT NULL DEFAULT md5(now()::text || (random() * 10000 + 1)::text),
+    key_get_param_name TEXT NOT NULL default 'api_key', 
+    is_https BOOLEAN NOT NULL DEFAULT false,
+    https_cert_file TEXT
+);
+
