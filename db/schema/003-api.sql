60b257224453aa5d11834cea7e04869695f4f9b9
Bloodcount <searosss@gmail.com>
2016-04-17 22:05:14 +0300

    refactor: removed empty files

--- db/schema/003-api.sql
+++
@@ -1,9 +0,0 @@
-create extension pgcrypto;
-
-CREATE TABLE apis(
-    id SERIAL PRIMARY KEY,
-    api_key TEXT NOT NULL DEFAULT md5(now()::text || (random() * 10000 + 1)::text),
-    key_get_param_name TEXT NOT NULL default 'api_key', 
-    is_https BOOLEAN NOT NULL DEFAULT false,
-    https_cert_file TEXT
-);
