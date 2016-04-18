8ffe77a23bfffd421c4c4cf156cc2b8b91692db6
Bloodcount <searosss@gmail.com>
2016-04-18 23:15:51 +0300

    feat: removed absolute paths

--- build_server.sh
+++ build_server.sh
@@ -1,10 +1,10 @@
 cd server
 echo "Running the automated tests..."
-/usr/local/go/bin/go test -coverprofile=c.out
+$GOROOT/bin/go test -coverprofile=c.out
 #echo "Generating HTML report..."
 #/usr/local/go/bin/go tool cover -html=c.out
 echo "Removing the test coverprofile file..."
 rm c.out
 echo "Building the executable..."
-/usr/local/go/bin/go build
+$GOROOT/bin/go build
 echo "Done!"
