d3aaca83a02c0a7358d930270a3aec57c821029f
Bloodcount <searosss@gmail.com>
2016-04-19 22:58:01 +0300

    feat: tests log output to stderr

--- build_server.sh
+++ build_server.sh
@@ -1,6 +1,6 @@
 cd server
 echo "Running the automated tests..."
-$GOROOT/bin/go test -coverprofile=c.out
+$GOROOT/bin/go test -coverprofile=c.out -logtostderr=true
 #echo "Generating HTML report..."
 #/usr/local/go/bin/go tool cover -html=c.out
 echo "Removing the test coverprofile file..."
