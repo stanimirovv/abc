c6b5fe32ce3ccca496d7b551caf1a97e7a246fa5
Bloodcount <searosss@gmail.com>
2016-04-30 00:04:06 +0300

    feat: delete on thread and thread_posts for test script

--- db/schema/999-perms.sql
+++ db/schema/999-perms.sql
@@ -11,3 +11,4 @@ GRANT SELECT    ON image_board_clusters TO abc_api;
 GRANT USAGE     ON image_board_clusters_id_seq TO abc_api;
 GRANT SELECT    ON thread_limits_reached_actions TO abc_api;
 GRANT USAGE     ON thread_limits_reached_actions_id_seq TO abc_api;
+GRANT delete on threads, thread_posts to abc_api;
