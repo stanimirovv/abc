52edfacd860d89e8612807dc8a01486e332a46c4
Bloodcount <searosss@gmail.com>
2016-05-13 23:34:02 +0300

    feat: default data for the unit tests

---
+++ db/schema/003-test_data.sql
@@ -0,0 +1,3 @@
+-- Data used by the testing
+INSERT INTO image_board_clusters(id, name, descr, board_limit_count, api_key) VALUES(1, 'test img board', 'asdasdasdasd', 99999, 'd3c3f756aff00db5cb063765b828e87b');
+INSERT INTO boards(id, name, image_board_cluster_id) VALUES(1, 'test board', 1);
